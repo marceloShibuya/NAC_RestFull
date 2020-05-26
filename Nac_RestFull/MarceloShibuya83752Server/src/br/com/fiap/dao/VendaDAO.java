46016451379e0602d16b51b9dad9c9a8197de041
marceloShibuya <50499486+marceloShibuya@users.noreply.github.com>
2020-05-26 16:41:54 -0300

    Primeiro Commit

---
+++ Nac_RestFull/MarceloShibuya83752Server/src/br/com/fiap/dao/VendaDAO.java
@@ -0,0 +1,97 @@
+package br.com.fiap.dao;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import br.com.fiap.to.VendaTO;
+
+public class VendaDAO {
+	
+	public static List<VendaTO> listaVendas = null;
+	 
+    public VendaDAO() {
+        
+        if(listaVendas == null) {
+            listaVendas = new ArrayList<VendaTO>();
+            
+            VendaTO vto = new VendaTO(1,"Pedro", new GregorianCalendar(2019,Calendar.DECEMBER,25), "Arvore de Natal",250.0, true);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(2,"Gustavo", new GregorianCalendar(2018,Calendar.JANUARY,02), "Celular",1500.0, true);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(3,"Marcelo", new GregorianCalendar(2020,Calendar.MARCH,21), "Caixa de Som",150.0, false);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(4,"Felipe", new GregorianCalendar(2020,Calendar.APRIL,01), "Microfone",100.0, true);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(5,"Luiz", new GregorianCalendar(2020,Calendar.MARCH,10), "Notebook",3990.0, false);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(6,"Josefina", new GregorianCalendar(2020,Calendar.APRIL,20), "Estojo de Maquiagem",259.0, true);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(7,"Gabriel", new GregorianCalendar(2019,Calendar.NOVEMBER,30), "Capa de Celular",25.0, true);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(8,"Rafael", new GregorianCalendar(2020,Calendar.FEBRUARY,15), "TV 50'",2590.0, false);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(9,"Emanuel", new GregorianCalendar(2019,Calendar.OCTOBER,03), "Fone de Ouvido",399.0, true);
+            listaVendas.add(vto);
+            
+            vto = new VendaTO(10,"Maria", new GregorianCalendar(2019,Calendar.JULY,19), "Impressora",999.0, true);
+            listaVendas.add(vto);
+        
+        }
+        
+    }
+	
+	
+	public List<VendaTO> select(){
+		return listaVendas;
+	}
+	
+	public VendaTO select(int cod) {
+		for(int x = 0;x < listaVendas.size();x++) {
+			if(listaVendas.get(x).getCodigo() == cod) {
+				return listaVendas.get(x);
+			}
+		}
+		return null;
+	}
+	
+	public boolean insert(VendaTO venda) {
+		
+		if(venda != null) {
+			venda.setCodigo((listaVendas.get(listaVendas.size()-1).getCodigo()+1));
+			return listaVendas.add(venda);
+		}
+		return false;
+	}
+	
+	public boolean update(VendaTO venda) {
+		
+		if(venda != null) {
+			for(int x = 0;x < listaVendas.size();x++) {
+				if(listaVendas.get(x).getCodigo() == venda.getCodigo()) {
+					listaVendas.set(x, venda);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public void delete(int cod) {
+		for(int x = 0;x < listaVendas.size();x++) {
+			if(listaVendas.get(x).getCodigo() == cod) {
+				listaVendas.remove(x);
+			}
+		}
+	}
+	
+}
---
+++ Nac_RestFull/MarceloShibuya83752Server/src/br/com/fiap/resource/VendaResource.java
@@ -0,0 +1,66 @@
+package br.com.fiap.resource;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import br.com.fiap.bo.VendaBO;
+import br.com.fiap.to.VendaTO;
+
+@Path("/venda")
+public class VendaResource {
+	
+	private VendaBO bo = new VendaBO();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<VendaTO> buscar(){
+		return bo.listagem();
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public VendaTO buscar(@PathParam("id")int cod) {
+		return bo.listagem(cod);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cadastrar(VendaTO venda,@Context UriInfo uriInfo) {
+		bo.cadastrarVenda(venda);
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+		builder.path(Integer.toString(venda.getCodigo()));
+		return Response.created(builder.build()).build();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response atualizar(VendaTO venda,@PathParam("id") int cod) {
+		venda.setCodigo(cod);
+		bo.atualizarVenda(venda);
+		return Response.ok().build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public void remover(@PathParam("id")int cod) {
+		bo.excluir(cod);
+	}
+	
+	
+
+}
